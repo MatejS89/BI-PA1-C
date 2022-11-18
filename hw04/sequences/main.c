e808ae2d7fc6dc33aa703ff12ea799b17d88e528
sedlam53 <sedlam53@fit.cvut.cz>
2022-11-18 22:31:44 +0100

    hw04: ProgTest04_Sequences, 3.5 point progtest solution

--- hw04/sequences/main.c
+++ hw04/sequences/main.c
@@ -2,83 +2,86 @@
 #include <stdlib.h>
 #define ARRAY_MAX 2000
 
-void sumOfElements( int *arrLength, int inputArray[], int sumArray[], int * sumArrLength )
+void insertSum( size_t * sumArrLength, int ** sumArray, int sum, size_t sumIndex)
 {
-	int q = 1;
-	int sum = inputArray[0];
-	for ( int i = 0; i <= *arrLength - 1;)
+	if ( sumIndex >= * sumArrLength )
 	{
-		for ( int n = q; n < *arrLength;)
-		{
-			sum += inputArray[n];
-			n++;
-			sumArray[*sumArrLength] = sum;
-			*sumArrLength += 1;
-		}
-
-		i++;
-		sum = inputArray[i];
-		q = i+1;
+	* sumArrLength = ( * sumArrLength ) * 2 + 1;
+	
+	*sumArray = ( int * )realloc( * sumArray, ( * sumArrLength ) * sizeof (int) );
 	}
+
+	( *sumArray )[sumIndex] = sum;
 }
 
-void checkSameElements( int sumArray[], int * sumArrLength, int * sameSum ) 
+int sumOfElements( int * arrLength, int inputArray[], int ** sumArray, size_t * sumArrLength )
 {
-	int q = 1;
-	for ( int i = 0; i < *sumArrLength - 1;)
+	int sum = inputArray[0];
+	size_t sumIndex = 0;
+	for ( int i = 0; i <= * arrLength - 1; i++ )
 	{
-		while ( q <= *sumArrLength - 1 )
+		for ( int n = i + 1; n < * arrLength; n++ )
 		{
-			if ( sumArray[i] == sumArray[q] )
+			sum += inputArray[n];
+			insertSum( sumArrLength, sumArray, sum, sumIndex );
+			sumIndex += 1;
+		}
+		sum = inputArray[i + 1];
+	}
+	return sumIndex;
+}
+
+void checkSameElements( int sumArray[], int * sumIndex, int * sameSum ) 
+{
+	for ( int i = 0; i < *sumIndex - 1; i++)
+	{
+		for ( int j = i + 1; j <= *sumIndex - 1; j++ )
+		{
+			if ( sumArray[i] == sumArray[j] )
 			{
 				*sameSum += 1;
 			}
-		q++;
 		}
-		i++;
-		q = i+1;
 	}
 	printf ( "Pocet dvojic: %d\n", *sameSum );
 }
 
 int main( void )
 {
-	int sumArrLength = 0;
-	int sumArray[2000];
+	size_t sumArrLength = 0;
   int inputArray[ARRAY_MAX];
   int arrLength = 0;
   int sameSum = 0;
+	int * sumArray = ( int * ) malloc ( 0 * sizeof( int ));
+
   printf ( "%s\n", "Posloupnost:" );
-  
 
 	while ( scanf ( "%d", &inputArray[arrLength] ) == 1 )
 	{
 		arrLength += 1;
 	}
 
-	if ( arrLength == 0 || arrLength > 2000 || ( ! feof (stdin) ) )
+	if ( arrLength == 0 || arrLength > 2000 || ( ! feof ( stdin ) ) )
 	{
 		printf ("%s\n", "Nespravny vstup.");
 		return 1;
 	}
 
-/*
-  for ( int n = 0; n < arrLength; ++n )
+
+/*  for ( int n = 0; n < arrLength; ++n )
   {
     printf ( "%d\n", inputArray[n] );
   }
 */  
 
-  sumOfElements( &arrLength, inputArray, sumArray, &sumArrLength );
-	
-	checkSameElements( sumArray, &sumArrLength, &sameSum );
-
-
-/*		for ( int p = 0; p < sumArrLength; )
+	int sumIndex = sumOfElements( &arrLength, inputArray, &sumArray, &sumArrLength );
+	checkSameElements( sumArray, &sumIndex, &sameSum );
+/*	
+	for ( int p = 0; p < sumIndex; )
 		{
 			printf("\n%d", sumArray[p]);
 			p++;
 		}
-*/
-
+*/	
+	free( sumArray );
 }
