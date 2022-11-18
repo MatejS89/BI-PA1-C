6ec83a2b3107b43df5e8d82d67f4445c406b1d8d
sedlam53 <sedlam53@fit.cvut.cz>
2022-11-18 13:50:04 +0100

    hw04: ProgTest04_Sequences, first functioning version

--- hw04/sequences/main.c
+++ hw04/sequences/main.c
@@ -2,23 +2,43 @@
 #include <stdlib.h>
 #define ARRAY_MAX 2000
 
-void sumOfElements( int *arrLength, int inputArray[], int sumArray[], int * sumArrLength)
+void sumOfElements( int *arrLength, int inputArray[], int sumArray[], int * sumArrLength )
 {
 	int q = 1;
-
-	for ( int i = 0; i < *arrLength - 1; )
+	int sum = inputArray[0];
+	for ( int i = 0; i <= *arrLength - 1;)
 	{
-		while ( q < *arrLength )
+		for ( int n = q; n < *arrLength;)
 		{
+			sum += inputArray[n];
+			n++;
+			sumArray[*sumArrLength] = sum;
+			*sumArrLength += 1;
+		}
+
+		i++;
+		sum = inputArray[i];
+		q = i+1;
+	}
+}
+
+void checkSameElements( int sumArray[], int * sumArrLength, int * sameSum ) 
+{
+	int q = 1;
+	for ( int i = 0; i < *sumArrLength - 1;)
+	{
+		while ( q <= *sumArrLength - 1 )
+		{
+			if ( sumArray[i] == sumArray[q] )
 			{
-				sumArray[*sumArrLength] = inputArray[i] + inputArray[q];
-				*sumArrLength += 1;
+				*sameSum += 1;
 			}
 		q++;
 		}
 		i++;
 		q = i+1;
 	}
+	printf ( "Pocet dvojic: %d\n", *sameSum );
 }
 
 int main( void )
@@ -26,31 +46,39 @@ int main( void )
 	int sumArrLength = 0;
 	int sumArray[2000];
   int inputArray[ARRAY_MAX];
-  int inputNum;
   int arrLength = 0;
-  
+  int sameSum = 0;
   printf ( "%s\n", "Posloupnost:" );
   
-  while ( scanf ("%d", &inputNum) == 1)
-  {
-    inputArray[arrLength] = inputNum;
-    ++arrLength;
-  }
 
-  /*for ( int n = 0; n < i; ++n )
+	while ( scanf ( "%d", &inputArray[arrLength] ) == 1 )
+	{
+		arrLength += 1;
+	}
+
+	if ( arrLength == 0 || arrLength > 2000 || ( ! feof (stdin) ) )
+	{
+		printf ("%s\n", "Nespravny vstup.");
+		return 1;
+	}
+
+/*
+  for ( int n = 0; n < arrLength; ++n )
   {
     printf ( "%d\n", inputArray[n] );
   }
-  return 0;
-  */
+*/  
 
-  sumOfElements( &arrLength, inputArray, sumArray, &sumArrLength);
+  sumOfElements( &arrLength, inputArray, sumArray, &sumArrLength );
 	
+	checkSameElements( sumArray, &sumArrLength, &sameSum );
 
-	for ( int p = 0; p < sumArrLength; )
+
+/*		for ( int p = 0; p < sumArrLength; )
 		{
-			printf("%d\n", sumArray[p]);
+			printf("\n%d", sumArray[p]);
 			p++;
 		}
+*/
 
 }
