b9eaf914084262bae028bc8c8facbab3bd54084a
sedlam53 <sedlam53@fit.cvut.cz>
2022-11-18 10:33:20 +0100

    hw04: ProgTest04_Sequences, add sum array made of elements of input array

--- hw04/sequences/main.c
+++ hw04/sequences/main.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define ARRAY_MAX 2000
-int main ( void )
+
+void sumOfElements( int *arrLength, int inputArray[], int sumArray[], int * sumArrLength)
 {
+	int q = 1;
+
+	for ( int i = 0; i < *arrLength - 1; )
+	{
+		while ( q < *arrLength )
+		{
+			{
+				sumArray[*sumArrLength] = inputArray[i] + inputArray[q];
+				*sumArrLength += 1;
+			}
+		q++;
+		}
+		i++;
+		q = i+1;
+	}
+}
+
+int main( void )
+{
+	int sumArrLength = 0;
+	int sumArray[2000];
   int inputArray[ARRAY_MAX];
   int inputNum;
-  int i = 0;
+  int arrLength = 0;
   
   printf ( "%s\n", "Posloupnost:" );
   
   while ( scanf ("%d", &inputNum) == 1)
   {
-    inputArray[i] = inputNum;
-    ++i;
+    inputArray[arrLength] = inputNum;
+    ++arrLength;
   }
 
-  for ( int n = 0; n < i; ++n )
+  /*for ( int n = 0; n < i; ++n )
   {
     printf ( "%d\n", inputArray[n] );
   }
   return 0;
+  */
+
+  sumOfElements( &arrLength, inputArray, sumArray, &sumArrLength);
+	
+
+	for ( int p = 0; p < sumArrLength; )
+		{
+			printf("%d\n", sumArray[p]);
+			p++;
+		}
+
 }
