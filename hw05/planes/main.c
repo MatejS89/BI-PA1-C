718797b63dfc4b8f0b56e26e44be5e24775209ec
sedlam53 <sedlam53@fit.cvut.cz>
2022-11-26 23:03:25 +0100

    hw05: ProgTest05_Planes, faster algorithm for pairs

--- hw05/planes/main.c
+++ hw05/planes/main.c
@@ -18,40 +18,36 @@ typedef struct
 	char planeName2[200];
 } Pairs;
 
-
 int loadStudent ( Plane * arr, unsigned long long countArr )
 {
 	int res = scanf ( "%lf,%lf: %199s", &arr[countArr].xCoord, &arr[countArr].yCoord, arr[countArr].planeName );
 	return res == 3;
 }
 
+/*
 int distCmp ( const void * d1, const void * d2 )
 {
-	if ( * ( double * )d1 > * ( double * )d2) 
-	return 1;
-	if ( * ( double * )d1 < * ( double * )d2)
-	return -1;
-	else
-	return 0;
+	return ( * ( double * )d1 - * ( double * )d2 );
 }
+*/
 
 int main ()
 {
 	Pairs *pairs;
 	Plane *arr;
 	unsigned long long countArr = 0;
-	unsigned long long capacityArr = 5;
+	unsigned long long capacityArr = 100;
 	unsigned long long pairCount = 0;
-	unsigned long long capacityPairs = 5;
+	unsigned long long capacityPairs = 100;
+	int inputCheck;
 	arr = ( Plane * ) malloc ( sizeof ( Plane ) * capacityArr );
 	pairs = ( Pairs * ) malloc ( sizeof ( Pairs ) * capacityPairs );
 
 	printf ( "%s\n", "Pozice letadel:" );
 
-	while ( !feof( stdin ) )
+	while ( ( inputCheck = scanf ( "%lf,%lf: %199s", &arr[countArr].xCoord, &arr[countArr].yCoord, arr[countArr].planeName) ) != EOF )
 	{
-		int inputCheck = loadStudent ( arr, countArr );
-		if ( inputCheck == 0 )
+		if ( inputCheck != 3 )
 		{
 			printf ( "%s\n", "Nespravny vstup.");
 			free ( pairs );
@@ -60,10 +56,19 @@ int main ()
 		}
 
 		countArr++;
-		if ( countArr == capacityArr - 1 )
+		
+		if ( countArr >= capacityArr - 1 )
 		{
-			capacityArr *= 2;
-			arr = ( Plane * ) realloc ( arr, sizeof ( Plane ) * capacityArr );
+			if ( capacityArr < 1000 )
+			{
+				capacityArr += 100;
+				arr = ( Plane * ) realloc ( arr, sizeof ( Plane ) * capacityArr );
+			}
+			else
+			{
+				capacityArr *= 3;
+				arr = ( Plane * ) realloc ( arr, sizeof ( Plane ) * capacityArr );
+			}
 		}
 	}
 
@@ -74,8 +79,16 @@ int main ()
 		free ( arr );
 		return 1;
 	}
+/*
+	for ( unsigned long long i = 0; i < countArr; i++ )
+	{
+		printf ( "%lf %lf %s", arr[i].xCoord, arr[i].yCoord, arr[i].planeName );
+	}
+*/
 
-	for ( unsigned long long i = 0; i <= countArr - 1; i++ )
+	double distMin;
+	double currentDist;
+	for ( unsigned long long i = 0; i < countArr - 1; i++ )
 	{
 		for ( unsigned long long q = i + 1; q < countArr; q++ )
 		{
@@ -84,48 +97,90 @@ int main ()
 			double x2 = arr[q].xCoord;
 			double y2 = arr[q].yCoord;
 
-			pairs[pairCount].dist = sqrt ( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1) );
-			strcpy ( pairs[pairCount].planeName1, arr[i].planeName );
- 			strcpy ( pairs[pairCount].planeName2, arr[q].planeName );
-			
-			pairCount ++;
-			if ( pairCount == capacityPairs - 1 )
+			currentDist = sqrt ( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1) );
+
+			if ( i == 0 && q == 1 )
 			{
-				capacityPairs *= 2;
-				pairs = ( Pairs * ) realloc ( pairs, sizeof ( Pairs ) * capacityPairs );
+				distMin = currentDist;
+			}
+
+			if ( pairCount >= capacityPairs - 1 )
+			{
+				if ( capacityPairs < 1000 )
+				{
+					capacityPairs += 100;
+					pairs = ( Pairs * ) realloc ( pairs, sizeof ( Pairs ) * capacityPairs );
+					if ( pairs == NULL)
+					{
+						printf ("%s\n","REALLOC FAILED1");
+					}
+				}
+				else
+				{
+					capacityPairs *= 2;
+					pairs = ( Pairs * ) realloc ( pairs, sizeof ( Pairs ) * capacityPairs );
+					if ( pairs == NULL)
+					{
+						printf ("%s\n","REALLOC FAILED2");
+					}
+				}
+			}
+
+			if ( fabs ( currentDist - distMin ) <= EPS * fabs ( currentDist + distMin ))
+			{
+				strcpy ( pairs[pairCount].planeName1, arr[i].planeName );
+ 				strcpy ( pairs[pairCount].planeName2, arr[q].planeName );
+				pairCount++;
+			}
+			
+			else if ( currentDist < distMin )
+			{
+				pairCount = 0;
+				distMin = currentDist;
+				free( pairs );
+				pairs = ( Pairs * ) malloc ( sizeof ( Pairs ) * capacityPairs );
+				strcpy ( pairs[pairCount].planeName1, arr[i].planeName );
+ 				strcpy ( pairs[pairCount].planeName2, arr[q].planeName );
+				pairCount++;
 			}
 		}
 	}
 	free ( arr );
-	qsort ( pairs, pairCount, sizeof ( Pairs ), distCmp );
+//qsort ( pairs, pairCount, sizeof ( Pairs ), distCmp );
+
+	/*for ( long long unsigned i = 0; i < pairCount; i++ )
+	{
+		printf ( "LIETADLO %llu %lf %s %s\n", i, pairs[i].dist, pairs[i].planeName1, pairs[i].planeName2 );
+	}
 
 	unsigned long long matchPairs = 0, i = 0;
 	double matchValue = pairs[0].dist;
 	
-	while ( pairs[i].dist - matchValue <= EPS * ( pairs[i].dist + matchValue ) )
+	while ( ( pairs[i].dist - matchValue <= EPS * ( pairs[i].dist + matchValue ) || pairs[i].dist == matchValue ) && ( i < pairCount ) )
 	{
 		matchPairs++;
 		i++;
 	}
-	printf ( "\nVzdalenost nejblizsich letadel: %lf\n", matchValue );
-	printf ( "Nalezenych dvojic: %lld\n", matchPairs );
-	for ( long long unsigned i = 0; i < matchPairs; i++ )
+*/
+
+	printf ( "Vzdalenost nejblizsich letadel: %lf\n", distMin );
+	printf ( "Nalezenych dvojic: %lld\n", pairCount );
+	for ( long long unsigned i = 0; i < pairCount; i++ )
 	{
 		printf ( "%s - %s\n", pairs[i].planeName1, pairs[i].planeName2 );
 	}
 
-/*
-	for ( size_t i = 0; i < count; i++ )
+	/*for ( size_t i = 0; i < count; i++ )
 	{
 		printf ( "%lf %lf %s\n", arr[i].xCoord, arr[i].yCoord, arr[i].planeName );
-	}
+	}*/
 
-	
+	/*
 	for ( unsigned long long i = 0; i < pairCount; i++ )
 	{
 		printf ( "%lf %s %s\n", pairs[i].dist, pairs[i].planeName1, pairs[i].planeName2 );
 	}
-	*/
-	free ( pairs );
+*/
+	free (pairs);
 	return 0;
 }
